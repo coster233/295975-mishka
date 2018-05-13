f89785ac7cc7168e414e9e9aa7276c33f3c0e59c
Artur <nocoster@yandex.ru>
2018-05-13 23:44:34 +0300

    на проверку

--- source/js/modal-window.js
+++ source/js/modal-window.js
@@ -1,35 +1,35 @@
 var modalwindow = document.querySelector(".modal");
 var modalclose = document.querySelector(".modal__button");
-var catalogLink = document.querySelectorAll(".catalog__svg-link");
+var catalogLink = document.querySelectorAll(".catalog-items__basket");
 var buttonopen = document.querySelector(".goods-week__order");
 
 
-if(buttonopen) {
-buttonopen.addEventListener("click", function (evt) {
-evt.preventDefault();
-modalwindow.classList.add("modal--opened");
+if (buttonopen) {
+    buttonopen.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        modalwindow.classList.add("modal--opened");
+    });
+}
+
+if (catalogLink) {
+    for (var j = 0; j < catalogLink.length; j++) {
+        catalogLink[j].addEventListener("click", function(evt) {
+            evt.preventDefault();
+            modalwindow.classList.add("modal--opened");
+        })
+    }
+}
+
+modalclose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modalwindow.classList.remove("modal--opened");
 });
-}
 
-if(catalogLink) {
-for (var j = 0; j < catalogLink.length; j++) {
-catalogLink[j].addEventListener("click", function (evt) {
-evt.preventDefault();
-modalwindow.classList.add("modal--opened");
-})
-}
-}
-
-modalclose.addEventListener("click", function (evt) {
-evt.preventDefault();
-modalwindow.classList.remove("modal--opened");
-});
-
-window.addEventListener("keydown", function (evt) {
-if (evt.keyCode === 27) {
-evt.preventDefault();
-if (modalwindow.classList.contains("modal--opened")) {
-modalwindow.classList.remove("modal--opened");
-}
-}
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (modalwindow.classList.contains("modal--opened")) {
+            modalwindow.classList.remove("modal--opened");
+        }
+    }
 });
